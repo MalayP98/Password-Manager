d7829c9aa1a0b4afa7443b5dfbed3adc634b483a
MalayP98 <tomalaypandey.tanu@gmail.com>
2022-11-01 22:10:04 +0530

    removed otp generetors(will add later if needed)

--- src/main/java/com/key/password_manager/stringgenerators/otpgenerators/OtpGenerator.java
+++
@@ -1,7 +0,0 @@
-package com.key.password_manager.stringgenerators.otpgenerators;
-
-import com.key.password_manager.stringgenerators.StringGenerator;
-
-public interface OtpGenerator extends StringGenerator {
-
-}
